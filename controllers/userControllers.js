cc43dde8d6d9e17322a7a25c4cc0c944bda08803
lbarbaqadze <161888275+lbarbaqadze@users.noreply.github.com>
2026-06-25 14:11:08 +0400

    fix: error handling status codes and file extensions

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -16,13 +16,13 @@ export const getUsersById = catchAsync(async (req, res, next) => {
     const { id } = req.params
 
     if(!id || isNaN(id)){
-        return next(new appError("wrong id format"), 400)
+        return next(new appError("wrong id format", 400))
     }
 
     const result = await userModel.findUserById(id)
 
     if(result.length === 0){
-        return next(new appError("data does not exist"), 404)
+        return next(new appError("data does not exist", 404))
     }
 
     res.status(200).json({
@@ -38,7 +38,7 @@ export const createUser = catchAsync(async (req, res, next) => {
     const checkUser = await userModel.existingUser(name, surname)
 
     if(checkUser.length > 0){
-        return next(new appError("data exist"), 409)
+        return next(new appError("data exist", 409))
     }
 
     const addUsers = await userModel.addUser(name, surname)
@@ -59,13 +59,13 @@ export const deleteUser = catchAsync(async (req, res, next) => {
     const { id } = req.params
 
     if(!id || isNaN(id)){
-        return next(new appError("wrong id format"), 400)
+        return next(new appError("wrong id format", 400))
     }
 
     const deleteuser = await userModel.deleteUserById(id)
 
     if(deleteuser.affectedRows === 0){
-        return next(new appError("data does not exist"), 404)
+        return next(new appError("data does not exist", 404))
     }
 
     res.status(200).json({
@@ -78,13 +78,13 @@ export const updateUser = catchAsync(async (req, res, next) => {
     const { name, surname } = req.body
 
     if(!id || isNaN(id)){
-        return next(new appError("wrong id format"), 400)
+        return next(new appError("wrong id format", 400))
     }
 
     const putUser = await userModel.updateUserById(name, surname, id)
 
     if(putUser.affectedRows === 0){
-        return next(new appError("data does not exist"), 404)
+        return next(new appError("data does not exist", 404))
     }
 
     res.status(200).json({
